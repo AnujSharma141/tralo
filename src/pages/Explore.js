b70a7f20f825ac569f78f9ce1c7e55e9c9855614
anujsharma141 <19ucc109@lnmiit.ac.in>
2023-08-23 21:02:57 +0530

    Fix: ENV Variable!

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,22 +1,12 @@
-import { Box, Card, Center, Flex, Input, Image, Heading, Text, Button, Avatar, MenuButton, Menu, MenuList, MenuItem, CloseButton } from '@chakra-ui/react'
-import {
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalFooter,
-    ModalBody,
-    ModalCloseButton,
-} from '@chakra-ui/react'
+import { Box, Center, Flex, Input } from '@chakra-ui/react'
+
 import React, { useEffect,useRef, useState } from 'react'
 
-import branding from '../assets/img/branding.png'
 import mapboxGl from 'mapbox-gl/dist/mapbox-gl.js'
 
 import data from '../data/properties.json'
 
 import Cards from '../components/Cards'
-import Nav from '../components/Nav'
 import ProprtyView from '../components/ProprtyView'
 
 
@@ -29,9 +19,8 @@ export default function Explore() {
 
     const list = data
 
-    mapboxGl.accessToken = process.env.MAP;
-    
     useEffect(()=>{
+    mapboxGl.accessToken = process.env.MAP;
     map.current = new mapboxGl.Map({
         container: 'map',
         center: [75.8338, 25.1638],
@@ -66,7 +55,6 @@ return (
             <Box w='40vw'>
 
             <Box position='fixed' bg='#F1F1F1' zIndex='1'> 
-                {/* <Image mt='3vw' src={branding} w='7vw' /> */}
                 <Input  fontSize='0.9vw' boxShadow='0 0 18px #ECECEC' background='white' borderColor='#C1C1C1' bgColor='white' padding='1.5vw 1vw' width='30vw' placeholder='search city ...' _placeholder={{color: '#A8A8A8'}}></Input>
             </Box>
 
@@ -76,7 +64,6 @@ return (
         </Box>
         
         <Box position='fixed' zIndex='1' top='0vw' right='0vw' id='map' h='100vh' bg='black' w='50vw'>
-            {/* <Nav /> */}
             {view.status? <ProprtyView view={view} close={setView} />:null}
         </Box>
 
